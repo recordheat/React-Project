ed5efbdff31d4608dda89bfcfac1bb4586573af0
Jacob Xu <113264182+recordheat@users.noreply.github.com>
2023-05-25 18:05:40 -0700

    Upload Commit
    Movie search application with API library of movies.

---
+++ first-react-project/src/App.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+import './App.css';
+
+import SearchIcon from './search.svg'
+
+import MovieCard from './MovieCard'
+
+const API_URL = 'https://www.omdbapi.com/?apikey=615a69b6';
+
+
+const App = () => {
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        setMovies(data.Search)
+    }
+    
+    
+    
+    useEffect(() => {    
+        searchMovies('Spiderman')
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>Movieland</h1>
+
+            <div className='search'>
+                <input placeholder='Search for movies' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}>
+                </input>
+                <img src={SearchIcon} alt='search' onClick={() =>searchMovies(searchTerm)}></img>
+            </div> 
+
+            
+                {movies?.length > 0 
+                ? (
+                <div className='container'>
+                    {movies.map((movie) => (
+                    <MovieCard movie={movie} />
+                    ))}
+                </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )}
+
+        
+
+        </div>
+    );
+};
+
+export default App;
